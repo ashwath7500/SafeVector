5f4b197b652280097858a6208d20d78aa2e47c3a
Ashwath S <ashwath.s@thoughtspot.com>
2022-12-15 00:57:42 +0100

    indendation fix

--- test.cpp
+++ test.cpp
@@ -1,11 +1,11 @@
 #include "safe_vector.hpp"
 
 int main() {
-	SafeVector<int> vec = {1,2,3,4,5};
+  SafeVector<int> vec = {1,2,3,4,5};
   auto it = vec.begin();
   ++it;
   cout<<*it<<"\n";
   vec.insert(vec.begin(),6);
-  cout<<*it;
-	return 0;
+	cout<<*it;
+  return 0;
 }
